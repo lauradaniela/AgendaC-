187b2535c7ac967409d15cc634268f9f571b2779
lauradaniela <lauradaniela92@gmail.com>
2018-03-26 16:42:32 -0500

    commit initial

---
+++ main.cs
@@ -0,0 +1,11 @@
+public class AgendaApplication
+{
+  static public void Main()
+  {
+    while (true) {
+      string x  = System.Console.ReadLine();
+      if (x == "exit"){ return; }
+      System.Console.WriteLine(x);
+    }
+  }
+}
